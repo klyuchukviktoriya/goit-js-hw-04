2ef68b9fdf45c53d0f55c42370d3696ef76d6e65
Klyuchuk Victoriya <154386083+klyuchukviktoriya@users.noreply.github.com>
2024-04-06 02:14:37 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const twoArrays = firstArray.concat(secondArray);
+    return twoArrays.length > maxLength ? twoArrays.slice(0, maxLength) : twoArrays;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
